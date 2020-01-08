6cc78fccbd667abde13a6fcd2869f125efafa58f
Dell <Dell@Dell-PC>
2020-01-08 11:01:26 +0530

    1st project

---
+++ src/main/java/demo3/abcdef/App.java
@@ -0,0 +1,13 @@
+package demo3.abcdef;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
